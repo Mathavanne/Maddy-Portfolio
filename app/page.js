c1749206642417beb1dc268b0976359daeea0507
Maddy <madymurugan.2u@gmail.com>
2024-12-29 13:39:31 +0530

    update

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 // import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { useEffect } from 'react';
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -21,7 +22,19 @@ async function getData() {
 
   return filtered;
 };
+export default function HomePage() {
+  useEffect(() => {
+      if (typeof document !== 'undefined') {
+          document.title = "Welcome to My Portfolio";
+      }
+  }, []);
 
+  return (
+      <div>
+          <h1>Welcome to My Portfolio</h1>
+      </div>
+  );
+}
 export default async function Home() {
   const blogs = await getData();
 
