9f761250bfab254b07e6eea214c2b6297cef7e33
Maddy <princemady216@gmail.com>
2025-02-25 13:06:07 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -6,36 +6,27 @@ import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react"
 import ScrollToTop from "./components/helper/scroll-to-top";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Mathavanne - Software Developer",
-};
+  title: "Portfolio of Mathavanne - Software Developer"};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Toast Notifications */}
         <ToastContainer />
-
-        {/* Main Content */}
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
           {children}
-          <Analytics />
+    <Analytics />
           <ScrollToTop />
         </main>
-
-        {/* Footer */}
         <Footer />
-
-        {/* Google Tag Manager (moved inside body) */}
-        {process.env.NEXT_PUBLIC_GTM && <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />}
       </body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
