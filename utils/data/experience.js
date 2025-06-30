2b64829dfe98b1eab5a92e9ff858cca8dc682b72
Mady <madymurugan.2u@gmail.com>
2025-07-01 00:14:37 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,12 +3,12 @@ export const experiences = [
     id: 1,
     title: 'Software Developer ',
     company: "Vilora Technologies",
-    duration: "(August 2024 - Present)"
+    duration: "(August 2024 - June 2025)"
   },
   {
     id: 2,
     title: "Mentor",
     company: "G-Tec Computer Education · Part-time",
-    duration: "(March 2025 - Present)"
+    duration: "(March 2025 - June 2025)"
   }
 ]
